36509da120f384f507acc5ec70c2802bcc99a869
Vadim Levin <vadimlevin1@gmail.com>
2022-09-07 15:11:34 +0300

    refac: update deprecated use_internal_diff

--- vim/.config/nvim/after/plugin/gitsigns.rc.lua
+++ vim/.config/nvim/after/plugin/gitsigns.rc.lua
@@ -64,7 +64,10 @@ gitsigns.setup({
     row = 0,
     col = 1
   },
-  use_internal_diff = true,  -- If vim.diff or luajit is present
+  diff_opts = {
+    internal = true
+  },
+--deprecated   use_internal_diff = true,  -- If vim.diff or luajit is present
   yadm = {
     enable = false
   },
